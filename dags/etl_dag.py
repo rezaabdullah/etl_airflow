b6bba76cb050b1f42a14400937ff42f53611213b
rezaabdullah <air.reza@hotmail.com>
2021-03-20 12:38:22 +0600

    feat: added PGCB etl script

--- dags/etl_dag.py
+++ dags/etl_dag.py
@@ -1,21 +1,22 @@
-from datetime import timedelta
+import datetime as dt
+from ..etl import etl
 
 # The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
 
 # Operators; we need this to operate!
-from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 # These args will get passed on to each operator
 # You can override them on a per-task basis during operator initialization
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'email': ['airflow@example.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    "owner": "abdullah reza",
+    "depends_on_past": False,
+    "email": ["air.reza@hotmail.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    'retry_delay': dt.timedelta(minutes=5),
     # 'queue': 'bash_queue',
     # 'pool': 'backfill',
     # 'priority_weight': 10,
@@ -30,3 +31,17 @@ default_args = {
     # 'sla_miss_callback': yet_another_function,
     # 'trigger_rule': 'all_success'
 }
+
+# schedule_interval=timedelta(days=1) 
+# with DAG("pgcb_etl", default_args=default_args, schedule_interval="@hourly") as dag:
+dag = DAG(
+    "pgcb_etl",
+    default_args=default_args,
+    schedule_interval="@hourly"
+)
+
+etl_task = PythonOperator(
+    task_id = "PGCB_ETL",
+    python_callable=etl.main,
+    dag=dag
+)
